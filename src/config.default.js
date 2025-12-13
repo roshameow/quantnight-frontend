0464690a93cf8f82959612acfb9584c8ccc087aa
roshameow <w.liuatnk@gmail.com>
2025-12-13 19:15:20 +0800

    add default config for release

---
+++ src/config.default.js
@@ -0,0 +1,21 @@
+export const AppConfig = {
+  "paths": {
+    "superTemplatePath": "./templates/super",
+    "templatePath": "./templates/regular",
+    "priorityTemplatePath": "./templates/priority"
+  },
+  "dataFilterOptions": [
+    {
+      "label": "alpha_submitted_zzz",
+      "value": "alpha_submitted_zzz"
+    },
+    {
+      "label": "alpha_results",
+      "value": "alpha_results"
+    },
+    {
+      "label": "alpha_submitted",
+      "value": "alpha_submitted"
+    }
+  ]
+};
